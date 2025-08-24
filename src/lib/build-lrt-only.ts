ac4dc602d938421a3ffaf7a37abc88265cd77a1f
nonissue <andy@nonissue.org>
2025-08-23 20:50:30 -0600

    add instruction for new slimming command

--- src/lib/build-lrt-only.ts
+++ src/lib/build-lrt-only.ts
@@ -1,4 +1,7 @@
+// npx tsx ./src/lib/build-lrt-only.ts --source ./db/gtfs.db --out ./data/gtfs_lrt_only.db --force
+
 /* eslint-disable no-console */
+
 import fs from 'node:fs';
 import path from 'node:path';
 import Database from 'better-sqlite3';
