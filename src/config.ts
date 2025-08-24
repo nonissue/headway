5bd18c389d4a05ff0c5c0843e8c88c7e1a5f9cbb
nonissue <andy@nonissue.org>
2025-08-23 20:50:36 -0600

    add variety of teset coords

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,37 @@
 export const SERVICE_DAY_START_HOUR = 5;
 export const SERVICE_DAY_END_HOUR = 4;
 export const STATION_SEARCH_BOUNDING_BOX_AREA = 100000;
+
+// Corona I think?
 export const TEST_COORDS = { lat: 53.542005, lon: -113.506607 };
+
 export const TEST_COORDS_SG = { lat: 53.485704, lon: -113.51379 };
+
+export const TEST_COORDS_HEALTH_SCIENCES_SOUTH = {
+    lat: 53.51806,
+    lon: -113.52569,
+};
+// (closest to / north of) mckernan belgravia 53.51473° N, 113.52580° W
+export const TEST_COORDS_MCKERNAN_BELGRAVIA = {
+    lat: 53.51473,
+    lon: -113.5258,
+};
+// (closest to / north of) south health campus 53.50584° N, 113.52845° W
+export const TEST_COORDS_SOUTH_HEALTH_CAMPUS_1 = {
+    lat: 53.50584,
+    lon: -113.52845,
+};
+// (closest to / south of) south health campus 53.49995° N, 113.52836° W
+export const TEST_COORDS_SOUTH_HEALTH_CAMPUS_2 = {
+    lat: 53.49995,
+    lon: -113.52836,
+};
+
+// South of century park
+export const TEST_COORDS_CENTURY_PARK = {
+    lat: 53.4425,
+    lon: -113.51827,
+};
 export const TEST_COORDS_FAR = { lat: 53.413853, lon: -113.634919 };
 export const TEST_COORDS_SUPER_FAR = { lat: 47.570042, lon: -52.681893 }; // signal hill
 export const DEFAULT_LOOK_AHEAD_IN_MINS = 120;
