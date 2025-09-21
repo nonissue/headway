1294d45628b92c8575af9c8bb7b8e7a3d0d9d64a
nonissue <andy@nonissue.org>
2025-09-21 17:31:23 -0600

    style tweaks

--- src/components/DeparturesTable.tsx
+++ src/components/DeparturesTable.tsx
@@ -16,36 +16,36 @@ export const DeparturesTable = ({
         <div
             key={departuresKey}
             className={cn(
-                'relative transition-all duration-500 ease-in-out',
+                'relative transition-all  ease-in-out ',
                 isTransitioning
-                    ? 'scale-[1] opacity-50 blur-[0px]'
-                    : 'blur-0 animate-in fade-in-0 scale-100 opacity-100 duration-700'
+                    ? 'scale-[1] opacity-0 blur-[10px]'
+                    : 'scale-100 animate-in opacity-100 blur-[0px] duration-500 fade-in-100'
             )}
         >
-            <div className="divide-foreground/20 relative space-y-0 divide-y border-y">
+            <div className="relative space-y-0 divide-y divide-foreground/20 border-y">
                 {processedDepartures.map((group, idx) =>
                     group.length == 0 ? (
                         <></>
                     ) : (
                         <div
                             key={`${departuresKey}-${idx}`}
-                            className="animate-in blur-in-0 relative flex w-full items-stretch"
+                            className="relative flex w-full animate-in items-stretch blur-in-0"
                             style={{
                                 animationDelay: `${idx * 1}ms`,
                             }}
                         >
-                            <div className="border-border/100 relative flex min-h-32 w-10 flex-col items-center justify-center border-r border-b-0 border-solid">
-                                <span className="text-muted-foreground rotate-[-90deg] text-xs font-bold tracking-[0.2em] whitespace-nowrap uppercase drop-shadow-xs">
+                            <div className="relative flex min-h-32 w-10 flex-col items-center justify-center border-r border-b-0 border-solid border-border/100">
+                                <span className="rotate-[-90deg] text-xs font-bold tracking-[0.2em] whitespace-nowrap text-muted-foreground uppercase drop-shadow-xs">
                                     Platform {idx + 1}
                                 </span>
                             </div>
 
-                            <div className="divide-foreground/20 flex-1 divide-y divide-dotted">
-                                <div className="text-foreground grid grid-cols-3 gap-2 bg-transparent px-4 py-2 text-xs font-bold tracking-wider uppercase">
-                                    <span className="text-muted-foreground font-[500]">
+                            <div className="flex-1 divide-y divide-dotted divide-foreground/20">
+                                <div className="grid grid-cols-3 gap-2 bg-transparent px-4 py-2 text-xs font-bold tracking-wider text-foreground uppercase">
+                                    <span className="font-[500] text-muted-foreground">
                                         Time
                                     </span>
-                                    <span className="text-muted-foreground col-span-2 font-[500]">
+                                    <span className="col-span-2 font-[500] text-muted-foreground">
                                         Destination
                                     </span>
                                 </div>
@@ -53,17 +53,17 @@ export const DeparturesTable = ({
                                     <div
                                         key={`${departuresKey}-${idx}-${i}`}
                                         className={cn(
-                                            'group animate-fade-in-up animate-duration-300 animate-ease-out grid grid-cols-3 gap-1 px-4 py-2 text-sm transition-all duration-200 ease-in-out',
-                                            'hover:bg-accent/20 hover:text-accent-foreground hover:cursor-pointer'
+                                            'group grid animate-in grid-cols-3 gap-1 px-4 py-2 text-sm opacity-0 duration-200 direction-reverse fade-in-100 fill-mode-forwards',
+                                            'hover:cursor-pointer hover:bg-accent/20 hover:text-accent-foreground'
                                         )}
                                         style={{
                                             animationDelay: `${idx * 100 + i * 50}ms`,
                                         }}
                                     >
-                                        <div className="text-primary/90 group-hover:text-accent-foreground my-auto font-mono text-xs font-[400] tracking-wider sm:text-sm">
+                                        <div className="my-auto font-mono text-xs font-[400] tracking-wider text-primary/90 group-hover:text-accent-foreground sm:text-sm">
                                             {dep.displayTime}
                                         </div>
-                                        <div className="text-chart-3 group-hover:text-accent-foreground font-display col-span-2 truncate text-sm font-[400] tracking-widest uppercase sm:text-base">
+                                        <div className="col-span-2 truncate font-display text-sm font-[600] tracking-widest text-chart-3 uppercase group-hover:text-accent-foreground sm:text-base">
                                             {dep.stop_headsign}
                                         </div>
                                     </div>
