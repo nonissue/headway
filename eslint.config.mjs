478b43aafe1ee101195e0793edd598a47dbbd288
nonissue <andy@nonissue.org>
2025-09-21 16:40:23 -0600

    i think it was related to vscode settings? not sure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,83 +1,94 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+// https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/280#issuecomment-2029302834
 
-// https://github.com/antfu/eslint-config#customization
-import antfu from "@antfu/eslint-config";
-
-// workaround for flat config not being supported yet by eslint-plugin-tailwindcss
-// https://github.com/francoismassart/eslint-plugin-tailwindcss/issues/280
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+
+// `eslint-plugin-tailwindcss` is not updated for tailwind v4 it seems
+// it relies on a tailwindcss lib (resolveConfig) which has been depreciated
+// so it cannot resolve config file. however, it seems to still work?
+import tailwind from "eslint-plugin-tailwindcss";
+import tselint from "typescript-eslint";
+import react from "eslint-plugin-react";
+// import eslintPluginTailwindCSS from 'eslint-plugin-tailwindcss';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    resolvePluginsRelativeTo: __dirname,
+});
 
-const compat = new FlatCompat();
-
-export default antfu(
+const eslintConfig = [
+    // ...compat.extends([
+    //     "eslint:recommended",
+    // "@typescript-eslint/recommended",
+    // "plugin:react/recommended",
+    //     "plugin:react-hooks/recommended",
+    // ]),
+    // ...compat.plugins("@typescript-eslint"),
+    // ...tselint.configs("flat/recommended"),
+    ...compat.plugins("@typescript-eslint"),
+    ...tselint.configs["recommended"],
+    ...compat.plugins("react"),
+    // ...react.configs.flat.recommended,
+    // ...react.configs.flat["jsx-runtime"],
+    ...compat.plugins("react-hooks"),
+    ...compat.plugins("tailwindcss"),
+    ...tailwind.configs["flat/recommended"],
     {
-        // Customize the stylistic rules
-        stylistic: {
-            quotes: "double", // or 'double'
-            semi: true,
-        },
-        rules: {
-            // Changing the default, we don't like this because when you rename ie. a field then things break
-            "object-shorthand": ["warn", "never"],
+        ...react.configs.flat.recommended,
+    },
+    {
+        settings: {
+            // ...react.configs.flat.recommended,
 
-            // We like our curly braces. Always.
-            "curly": ["warn", "all"],
-
-            // Keep line length below 120 characters
-            "max-len": ["error", { code: 120 }],
-
-            // Experimenting too much for now.
-            // TODO evaluate if we need to enable this again
-            "no-console": "warn",
-        },
-        formatters: {
-            graphql: "prettier",
-            css: "prettier",
-            markdown: "prettier",
-            html: "prettier",
-            prettierOptions: {
+            react: {
+                version: "detect",
+                // ...react.configs.flat.recommended,
+            },
+            eslintPluginTailwindCSS: {
+                config: `${__dirname}/src/globals.css`,
+                callees: ["cva", "cn", "classnames"],
+                cssFiles: [
+                    "**/*.css",
+                    "!**/node_modules",
+                    "!**/.*",
+                    "!**/dist",
+                    "!**/build",
+                ],
+            },
+            tailwindcss: {
+                config: `${__dirname}/src/globals.css`,
+                callees: ["cva", "cn", "classnames"],
+                cssFiles: [
+                    "**/*.css",
+                    "!**/node_modules",
+                    "!**/.*",
+                    "!**/dist",
+                    "!**/build",
+                ],
             },
         },
-    },
-    {
-        files: ["*.json"],
         rules: {
-            "max-len": "off",
-        },
-    },
-    ...compat.config({
-        // https://github.com/francoismassart/eslint-plugin-tailwindcss
-        extends: ["plugin:tailwindcss/recommended"],
-        settings: {
-            tailwindcss: {
-                config: `${__dirname}/src/globals.css`
-            }, 
-            callees: ['cva', 'cn', 'classnames', 'clsx'],
-            cssFiles: [
-                'src/globals.css',
-                'src/**/*.css',
-                '!**/node_modules',
-                '!**/.*',
-                '!**/dist',
-                '!**/build',
-            ],
-        },
-        rules: {
-            'tailwindcss/classnames-order': 'warn',
-            'tailwindcss/no-unnecessary-arbitrary-value': 'warn',
             'tailwindcss/enforces-shorthand': 'warn',
+            'tailwindcss/no-arbitrary-value': 'warn',
             'tailwindcss/no-contradicting-classname': 'warn',
             'tailwindcss/no-custom-classname': 'warn',
-            // Allow arbitrary values in Tailwind v4
-            'tailwindcss/no-arbitrary-value': 'off',
+            'tailwindcss/no-unnecessary-arbitrary-value': 'warn',
+            "tailwindcss/classnames-order": "warn",
+            "react/prop-types": "off",
+            "react/react-in-jsx-scope": "off",
+   
         },
-    }),
-    {
-        ignores: ["**/generated.*", "**/locales/generated/**"],
     },
-);
+    {
+        files: ["src/**/*.{ts,tsx}"],
+    },
+    {
+        ignores: ["dist/", "src/generated/**/*", "public/", "node_modules/"],
+    },
+];
+
+export default eslintConfig;
